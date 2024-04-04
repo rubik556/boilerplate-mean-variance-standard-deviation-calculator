a63c9683410cf355355b688a23dcce63fd542641
Jacobo Ruiz <jacoboruizcalleja@gmail.com>
2024-04-04 17:28:53 +0000

    Working stage

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,30 @@
 import numpy as np
 
-def calculate(list):
+def calculate(lst):
+    # 
+    if len(lst) != 9: raise ValueError("List must contain nine numbers.")
+    # Parsint
+    arr = np.array(lst).reshape((3,3))
+    d = {'mean':[],
+         'variance':[],
+         'standard deviation':[],
+         'max':[],
+         'min':[],
+         'sum':[] }
+
+    # Mean
+    d["mean"] = [arr.mean(axis=n).tolist() for n in [0,1,None]]
+    # variance
+    d["variance"] = [arr.var(axis=n).tolist() for n in [0,1,None]]
+    # standard deviation
+    d["standard deviation"] = [arr.std(axis=n).tolist() for n in [0,1,None]]
+    # max
+    d["max"] = [arr.max(axis=n).tolist() for n in [0,1,None]]
+    # min
+    d["min"] = [arr.min(axis=n).tolist() for n in [0,1,None]]
+    # sum
+    d["sum"] = [arr.sum(axis=n).tolist() for n in [0,1,None]]
 
 
 
-
-    return calculations
+    return d
